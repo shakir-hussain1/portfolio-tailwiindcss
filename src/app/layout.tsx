7570c8e912e3cd3ef4e07788bd6a941826285669
Shakir Hussain <161478273+shakir-hussain1@users.noreply.github.com>
2025-01-19 18:00:50 +0500

    tweak and updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import Footer from "@/components/Footer";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
